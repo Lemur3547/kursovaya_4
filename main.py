ca989d0ff0a7e1c0b62db0ff1d647937ac4f3092
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-04-08 22:19:58 +0500

    Создан класс для вакансий

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-from src.classes import HeadHunterAPI
+import json
+
+from src.classes import HeadHunterAPI, Vacancy
 
 vacancies = HeadHunterAPI()
 vacancies.get_vacancies('Python')
+
+with open('data/vacancies.json', 'rt', encoding='utf-8') as file:
+    vacancies = json.loads(file.read())
+
+vac = vacancies['items'][0]
+new_vac1 = Vacancy(vac['name'], vac['area']['name'], None, vac['address'], vac['employer'], vac['snippet'],
+                   vac['contacts'], vac['schedule']['name'], vac['professional_roles'], vac['experience']['name'],
+                   vac['alternate_url'])
+
+new_vac2 = Vacancy(vac['name'], vac['area']['name'], 120000, vac['address'], vac['employer'], vac['snippet'],
+                   vac['contacts'], vac['schedule']['name'], vac['professional_roles'], vac['experience']['name'],
+                   vac['alternate_url'])
--- src/classes.py
+++ src/classes.py
@@ -19,3 +19,36 @@ class HeadHunterAPI(APIVacancies):
         vacancies = response.json()
         with open('data/vacancies.json', 'wt', encoding='utf-8') as file:
             json.dump(vacancies, file, ensure_ascii=False)
+
+
+class Vacancy:
+    def __init__(self, name, city, salary, address, employer, description, contacts, schedule, professional_roles,
+                 experience, url):
+        self.name = name
+        self.city = city
+        if self.salary is None:
+            self.salary = 'Не указана'
+        else:
+            self.salary = salary
+        self.address = address
+        self.employer = employer
+        self.description = description
+        self.contacts = contacts
+        self.schedule = schedule
+        self.professional_roles = professional_roles
+        self.experience = experience
+        self.url = url
+
+    def __repr__(self):
+        return (f"{self.__class__.__name__}({self.name}, {self.city}, {self.salary}, {self.address}, {self.employer}, "
+                f"{self.description}, {self.contacts}, {self.schedule}, {self.professional_roles}, {self.experience}, {self.url})")
+
+    def __lt__(self, other):
+        if self.salary < other.salary:
+            return True
+        return False
+
+    def __gt__(self, other):
+        if self.salary > other.salary:
+            return True
+        return False
