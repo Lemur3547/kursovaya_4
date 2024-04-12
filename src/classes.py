b01d46d2d65c8a21721bf8bd095d3473de30727c
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-04-12 22:03:08 +0500

    Доработаны методы сравнения вакансий по зарплате. Создан класс для действий над вакансиями, наследующийся от абстрактного класса.

--- src/classes.py
+++ src/classes.py
@@ -1,4 +1,5 @@
 import json
+import re
 from abc import ABC, abstractmethod
 
 import requests
@@ -18,7 +19,7 @@ class HeadHunterAPI(APIVacancies):
         response = requests.get(self.hh_api, params={'text': name})
         vacancies = response.json()
         with open('data/vacancies.json', 'wt', encoding='utf-8') as file:
-            json.dump(vacancies, file, ensure_ascii=False)
+            json.dump(vacancies['items'], file, ensure_ascii=False)
 
 
 class Vacancy:
@@ -26,7 +27,7 @@ class Vacancy:
                  experience, url):
         self.name = name
         self.city = city
-        if self.salary is None:
+        if salary is None:
             self.salary = 'Не указана'
         else:
             self.salary = salary
@@ -41,14 +42,86 @@ class Vacancy:
 
     def __repr__(self):
         return (f"{self.__class__.__name__}({self.name}, {self.city}, {self.salary}, {self.address}, {self.employer}, "
-                f"{self.description}, {self.contacts}, {self.schedule}, {self.professional_roles}, {self.experience}, {self.url})")
+                f"{self.description}, {self.contacts}, {self.schedule}, {self.professional_roles}, {self.experience}, "
+                f"{self.url})")
 
     def __lt__(self, other):
-        if self.salary < other.salary:
-            return True
+        if self.salary != 'Не указана' and other.salary != 'Не указана':
+            if self.salary["from"] is not None and other.salary["from"] is not None:
+                if self.salary['from'] < other.salary['from']:
+                    return True
+                return False
+            return False
         return False
 
     def __gt__(self, other):
-        if self.salary > other.salary:
-            return True
+        if self.salary != 'Не указана' and other.salary != 'Не указана':
+            if self.salary["from"] is not None and other.salary["from"] is not None:
+                if self.salary['from'] > other.salary['from']:
+                    return True
+                return False
+            return False
         return False
+
+
+class VacanciesActions(ABC):
+
+    @abstractmethod
+    def add_vacancy(self, vacancy):
+        pass
+
+    @abstractmethod
+    def get_vacancy(self, tags):
+        pass
+
+    @abstractmethod
+    def del_vacancy(self, vacancy_id):
+        pass
+
+
+class VacanciesFile(VacanciesActions):
+
+    def __init__(self, directory='data/vacancies.json'):
+        self.directory = directory
+
+    def add_vacancy(self, new_vacancy):
+        with open(self.directory, 'rt', encoding='utf-8') as file:
+            vacancies_list = json.load(file)
+        with open(self.directory, 'wt', encoding='utf-8') as file:
+            vacancies_list.append(new_vacancy)
+            json.dump(vacancies_list, file, ensure_ascii=False)
+
+    def get_vacancy(self, tags):
+        tags = re.sub(r"[,:;.!?]", "", tags)
+        tags_list = tags.split()
+        with open(self.directory, 'rt', encoding='utf-8') as file:
+            vacancies = json.load(file)
+            if tags_list:
+                suitable_vacancies = []
+                for vacancy in vacancies:
+                    for tag in tags_list:
+                        if tag.lower() in vacancy['name'].lower():
+                            if vacancy not in suitable_vacancies:
+                                suitable_vacancies.append(vacancy)
+                        if vacancy["snippet"]["requirement"] is not None:
+                            if tag.lower() in vacancy["snippet"]["requirement"].lower():
+                                if vacancy not in suitable_vacancies:
+                                    suitable_vacancies.append(vacancy)
+                        if vacancy["snippet"]["responsibility"] is not None:
+                            if tag.lower() in vacancy["snippet"]["responsibility"].lower():
+                                if vacancy not in suitable_vacancies:
+                                    suitable_vacancies.append(vacancy)
+                        if tag.lower() in vacancy["professional_roles"][0]["name"].lower():
+                            if vacancy not in suitable_vacancies:
+                                suitable_vacancies.append(vacancy)
+                return suitable_vacancies
+            return vacancies
+
+    def del_vacancy(self, vacancy_id):
+        with open(self.directory, 'rt', encoding='utf-8') as file:
+            vacancies = json.load(file)
+        with open(self.directory, 'wt', encoding='utf-8') as file:
+            for vacancy in vacancies:
+                if str(vacancy_id) == vacancy['id']:
+                    vacancies.remove(vacancy)
+            json.dump(vacancies, file, ensure_ascii=False)
